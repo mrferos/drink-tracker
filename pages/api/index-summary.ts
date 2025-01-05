d7b6df0171c7463afc700b2974263219517e87cb
Andres Galindo <me@andresgalindo.us>
2025-01-05 18:55:15 -0500

    Added chart

--- pages/api/index-summary.ts
+++ pages/api/index-summary.ts
@@ -1,8 +1,63 @@
 import db from "../../lib/db";
+import moment from "moment";
+
+const daySecs = 86400;
 
 export default async function handler(req: any, res: any) {
     const time = Date.now()/1000;
-    const daySecs = 86400;
+    const { daySum, weekSum, monthSum } = await getCounts(time)
+
+    const monthDaily = await getMonthDaily(time)
+
+    return res.status(200).json({
+        monthDaily: monthDaily,
+        daySum,
+        weekSum,
+        monthSum,
+    })
+}
+
+const getMonthDaily = async (time: number): Promise<{date: string, sum: number}[]> => {
+    const greaterThanTime = moment.unix(time - (daySecs * 30))
+    const sumsByDay = await db("session_drinks")
+        .select(
+            db.raw("strftime('%Y-%m-%d', time, 'unixepoch') as day"),
+            db.raw('count(id) as sum')
+        )
+        .where("time", ">", greaterThanTime.unix())
+        .groupBy("day")
+
+    const sumMap = sumsByDay.reduce((map, item) => {
+        map[item.day] = item.sum;
+        return map
+    }, {})
+
+    const allDaysMap = {}
+    const last = moment.unix(time)
+    let cur = greaterThanTime
+    while(cur.isBefore(last)) {
+        const prettyDate = cur.format("YYYY-MM-DD")
+        if(sumMap[prettyDate]) {
+            allDaysMap[prettyDate] = sumMap[prettyDate]
+        } else {
+            allDaysMap[prettyDate] = 0
+        }
+
+        cur = cur.add(1, 'day')
+    }
+
+    const results = []
+    Object.entries(allDaysMap).forEach(([key, value]) => {
+        results.push({
+            date: key,
+            sum: value
+        })
+    })
+
+    return results
+}
+
+const getCounts = async (time: number): Promise<{daySum: number, weekSum: number, monthSum: number}> => {
     const {sum:daySum} = await db("session_drinks")
         .count("id as sum")
         .where("time",  ">",  time-daySecs)
@@ -18,9 +73,9 @@ export default async function handler(req: any, res: any) {
         .where("time",  ">",  time-(daySecs * 30))
         .first()
 
-    return res.status(200).json({
+    return {
         daySum,
         weekSum,
         monthSum,
-    })
+    }
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,9 @@
 import {Card} from "react-bootstrap";
 import {useEffect, useState} from "react";
+import {CartesianGrid, Line, LineChart, ResponsiveContainer, XAxis, YAxis} from "recharts";
 
 interface Sums {
+    monthDaily: {date: string, sum: number}[];
     daySum: number;
     weekSum: number;
     monthSum: number;
@@ -9,6 +11,7 @@ interface Sums {
 
 export default function Index() {
     const [sums, setSums] = useState<Sums>({
+        monthDaily: [],
         daySum: 0,
         weekSum: 0,
         monthSum: 0,
@@ -45,6 +48,19 @@ export default function Index() {
                 <Card.Title>Drinks in the last 30 days...</Card.Title>
                 <Card.Body>
                     <Card.Text><h1>{sums.monthSum}</h1></Card.Text>
+                </Card.Body>
+            </Card>
+            <Card style={{marginTop: '5px', padding: '5px'}}>
+                <Card.Title>Daily drinks over the last 30 days</Card.Title>
+                <Card.Body>
+                    <ResponsiveContainer width="100%" height={200}>
+                        <LineChart data={sums.monthDaily} height={200}>
+                            <CartesianGrid strokeDasharray="3 3" />
+                            <XAxis dataKey="date" />
+                            <YAxis />
+                            <Line type="monotone" dataKey="sum" />
+                        </LineChart>
+                    </ResponsiveContainer>
                 </Card.Body>
             </Card>
         </div>
